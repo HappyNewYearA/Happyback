cfe4c83b285dfbff7b2623541cb2241f7e70a179
mowang-mw <mowang@whu.edu.cn>
2024-07-17 11:54:33 +0800

    图片上传

--- src/main/java/com/happynewyeara/controller/add_img_controller.java
+++ src/main/java/com/happynewyeara/controller/add_img_controller.java
@@ -1,21 +1,25 @@
-package com.happynewyeara.controller;
-
-import com.happynewyeara.service.add_img_service;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class add_img_controller {
-    @Autowired
-    private add_img_service add_img_service;
-
-    @RequestMapping("/api/img/add")
-    public String register(HttpServletRequest request) {
-        String phone_num = request.getParameter("phone_num");
-        String scene_name = request.getParameter("scene_name");
-        String url = request.getParameter("url");;
-        return add_img_service.Addimg(phone_num, scene_name, url);
-    }
-}
+//package com.happynewyeara.controller;
+//
+//import com.happynewyeara.service.add_img_service;
+//import com.happynewyeara.utils.AliOSSUtils;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.IOException;
+//
+//@RestController
+//public class add_img_controller {
+//    @Autowired
+//    private add_img_service add_img_service;
+//    @Autowired
+//    private AliOSSUtils aliOSSUtils;
+//
+//    @RequestMapping("/api/img/add")
+//    public String register(String phone_num, String scene_name, MultipartFile img) throws IOException {
+//        String url = aliOSSUtils.upload(img);
+//        return add_img_service.Addimg(phone_num, scene_name, url);
+//    }
+//}
